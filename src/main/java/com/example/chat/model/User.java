9285caaf0957333527ca31317045a4489bd0668e
Dan Koller <57103678+dan-koller@users.noreply.github.com>
2023-04-15 13:59:03 +0200

    Removed user store (not needed in this demo app to keep it simple)

--- src/main/java/com/example/chat/model/User.java
+++ src/main/java/com/example/chat/model/User.java
@@ -3,7 +3,6 @@ package com.example.chat.model;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.mongodb.core.mapping.Document;
 
 /**
  * This class is used to represent a user in the chat.
@@ -11,7 +10,6 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
-@Document(collection = "users")
 public class User {
     private String username;
 }
